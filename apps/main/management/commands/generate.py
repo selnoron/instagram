8b62b86565b93e05706172a9de4b634572057b86
Danial <selnoron70@gmail.com>
2023-10-17 21:13:25 +0600

    generate_update

--- apps/main/management/commands/generate.py
+++ apps/main/management/commands/generate.py
@@ -1,6 +1,7 @@
-from main.models import MyUser  
+from main.models import MyUser 
 from faker import Faker
 from django.core.management.base import BaseCommand, CommandError
+import random as rd
 
 
 fake = Faker()
@@ -8,17 +9,18 @@ fake = Faker()
 class Command(BaseCommand):
     def generate_users(self):
         user_list = []
-        for i in range(1000000):
-            user = MyUser(
-                email=fake.email(),
+        n = 570000
+        for i in range(n):
+            print("\033[H\033[J")
+            print(round((i/n)*100, 1),'%', 'I'*int((i/n)*100) + '_'*int(100 - int((i/n)*100)))
+            MyUser.objects.create(
+                email=rd.choice(['df','de', 'ec', 'e', 'd', 'w', 'dds'])+str(rd.randint(1, 99999))+fake.email(),
                 nickname=fake.user_name(),
                 description=fake.sentence(),
-                is_staff=False 
+                is_staff=False
             )
-            user_list.append(user)
-
-        MyUser.objects.bulk_create(user_list)
 
     def handle(self, *args, **kwargs):
+        print("Start processings")
         self.generate_users()
-        print("FINISH")
+        print("Complete")
